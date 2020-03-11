b6884f23eec6b1075c7f1a017a581974c2768c9f
Jeidison Farias <jeidison.farias@gmail.com>
2020-03-11 16:39:45 -0300

    Suporte a todos os operadores no where.

--- src/Filtrable.php
+++ src/Filtrable.php
@@ -10,7 +10,7 @@ use ReflectionException;
 
 /**
  * @author Jeidison Farias <jeidison.farias@gmail.com>
- * @method static filter($inputs);
+ * @method static filter($inputs = []);
  */
 trait Filtrable
 {
@@ -18,9 +18,9 @@ trait Filtrable
      * @return array
      * @throws ReflectionException
      */
-    public static function columns()
+    public static function columnsFiltrable()
     {
-        $class = new ReflectionClass(self::class);
+        $class    = new ReflectionClass(self::class);
         $property = $class->getProperty('fillable');
         $property->setAccessible(true);
 
@@ -29,10 +29,10 @@ trait Filtrable
 
     /**
      * @param Builder $query
-     * @param $inputs
+     * @param array $inputs
      * @return Builder
      */
-    public function scopeFilter($query, $inputs = [])
+    public function scopeFilter(Builder $query, array $inputs = [])
     {
         $inputs = (!empty($inputs) ? $inputs : request()->all());
         $query  = $this->addFilter($query, $inputs);
@@ -43,13 +43,25 @@ trait Filtrable
         return $this->addOrderBy($query, $inputs);
     }
 
+    //TODO: Melhorar, e deixar recursivo
     private function addFilterRelationship(Builder $builder, array $inputs)
     {
         Arr::where($inputs, function ($value, $key) use ($builder) {
             if (Str::contains($key, '->')) {
-                $relation = Arr::first(explode('->', $key));
-                $builder->whereHas($relation, function (Builder $builder) use ($key, $value) {
-                    $builder->where(Str::replaceFirst('->', '.', $key), $value);
+                $keyExploded = explode('->', $key);
+                $relation = Arr::first($keyExploded);
+                $builder->whereHas($relation, function (Builder $query) use ($key, $value, $keyExploded) {
+                    if (count($keyExploded) > 2) {
+
+                        $relation = explode('->', Arr::get(explode('->', $key, 2), 1));
+                        $query->whereHas(Arr::first($relation), function (Builder $query2) use ($value, $key) {
+                            $collum = Arr::get(explode('->', $key, 2), 1);
+                            $query2->where(Str::replaceFirst('->', '.', $collum), $value);
+                        });
+
+                    } else {
+                        $query->where(Str::replaceFirst('->', '.', $key), $value);
+                    }
                 });
             }
         });
@@ -59,21 +71,25 @@ trait Filtrable
 
     private function addFilter(Builder $builder, array $inputs): Builder
     {
-        $builder = $this->addLike($builder, $inputs);
-        $filter  = Arr::only($inputs, self::columns());
+        $builder = $this->addOperator($builder, $inputs);
+        $filter  = Arr::only($inputs, $builder->getModel()::columnsFiltrable());
 
         return $builder->where($filter);
     }
 
-    private function addLike(Builder $builder, array $inputs): Builder
+    private function addOperator(Builder $builder, array $inputs): Builder
     {
         Arr::where($inputs, function ($value, $key) use ($builder) {
-            if (Str::contains($key, ':like')) {
-                $column = Arr::first(explode(':like', $key));
-                $column = Arr::only([$column => $value], self::columns());
-                if (!empty($column)) {
-                    $builder->where(key($column), 'like', $value);
-                }
+            if (!Str::contains($key, ':')) {
+                return;
+            }
+
+            $keyExploded = explode(':', $key);
+            $column        = Arr::first($keyExploded);
+            $column        = Arr::only([$column => $value], $builder->getModel()::columnsFiltrable());
+            if (!empty($column)) {
+                $operator = Arr::get($keyExploded, 1);
+                $builder->where(key($column), $operator, $value);
             }
         });
 
@@ -82,10 +98,10 @@ trait Filtrable
 
     private function addField(Builder $builder, array $inputs): Builder
     {
-        $fields = Arr::get($inputs, 'fields',  ['*']);
-        $fields = empty($fields) ? [] : array_map('trim', explode(',', $fields));
+        $fields = Arr::get($inputs, 'fields');
+        $fields = empty($fields) ? ['*'] : array_map('trim', explode(',', $fields));
 
-        return $builder->select($fields ?? ['*']);
+        return $builder->select($fields);
     }
 
     private function addWith(Builder $builder, array $inputs): Builder
