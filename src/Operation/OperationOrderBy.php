2bed8a3f0b0b210ad8f192149752f1e5b83ab4d7
Jeidison Farias <jeidison.farias@gmail.com>
2020-06-30 21:42:11 -0300

    add support to multiples order and order direction.

--- src/Operation/OperationOrderBy.php
+++ src/Operation/OperationOrderBy.php
@@ -10,8 +10,18 @@ class OperationOrderBy implements Operation
 
     public function addOperation(Builder $builder, array $inputs): Builder
     {
-        $order = Arr::get($inputs, 'order');
+        $orderStr = Arr::get($inputs, 'order');
+        if (!$orderStr)
+            return $builder->orderBy($builder->getModel()->getKeyName());
 
-        return $builder->orderBy($order ?? $builder->getModel()->getKeyName());
+        $orderList = explode(',', $orderStr);
+        foreach ($orderList as $orderField) {
+            $order     = explode(':', $orderField);
+            $field     = Arr::first($order);
+            $direction = (count($order) > 1) ? Arr::last($order) : 'asc';
+            $builder->orderBy($field, $direction);
+        }
+
+        return $builder;
     }
 }
